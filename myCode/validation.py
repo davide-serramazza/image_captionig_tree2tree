452af550a4e2da12cadbb9685de5c6db5a6d2b11
davide <davide.serramazza@gmail.com>
2021-01-26 17:53:50 +0100

    coeff. penaliz. encoder representation messo come input da terminale e tolto word embedding dimension (deve essere uguale a tree per come fatto NIC) #TODO stringa in val per tensorboard da passare a train; in tensorboard plottare norma di pesi e di encoder representation; controllare numero occorrenze per la quale ho 5k categorie;

--- myCode/validation.py
+++ myCode/validation.py
@@ -22,17 +22,16 @@ def main():
     parser.add_argument('CNN_to_use', type=str, help="CNN used to encoding the images (in case of image input type None).\
                         The two valid options are inception and alexnet")
     #TODO fare argomenti opzionali
-    parser.add_argument('emb_tree_size', type=int, help="size of tree embedding i.e. hidden coefficient")
+    parser.add_argument('emb_tree_word_size', type=int, help="dimension of tree embedding and word embedding (must be equal in NIC)")
     max_node_count = 100
     max_depth =  10
     cut_arity = 4
-    lambd= 0.000
+    parser.add_argument('lambd', type=float, help="coefficient of penalization on encoder representation")
     parser.add_argument('beta', type=float, help="coefficient for L2 reg.")
     parser.add_argument('hidden_coeff', type=float, help="coefficient for hidden dimension of encoder and decoder layer")
     learning_rate = 0.001
     clipping = 0.02
     batch_size = 32
-    parser.add_argument('emb_word_size', type=int, help="size of word embedding i.e. hidden coefficient")
     #TODO da mettere come float se tenuto come coefficiente o come int se a se stante
     parser.add_argument('rnn_unit_size', type=int, help="size of rnn expressed as coefficient relative to embedding size")
     keep_rate=1.0
@@ -60,9 +59,9 @@ def main():
     image_max_arity, sen_max_arity = compute_max_arity(train_data, val_data)
 
     activation = getattr(tf.nn, FLAGS.activation)
-    decoder, encoder = get_encoder_decoder(emb_tree_size=args.emb_tree_size,cut_arity=cut_arity,max_arity=
+    decoder, encoder = get_encoder_decoder(emb_tree_size=args.emb_tree_word_size,cut_arity=cut_arity,max_arity=
         max(image_max_arity,sen_max_arity),max_node_count=max_node_count,max_depth=max_depth,hidden_coeff=args.hidden_coeff,
-        activation=activation,image_tree=image_tree,sentence_tree=sentence_tree,emb_word_size=args.emb_word_size,
+        activation=activation,image_tree=image_tree,sentence_tree=sentence_tree,emb_word_size=args.emb_tree_word_size,
         hidden_word=args.rnn_unit_size,keep_rate=keep_rate)
 
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
@@ -70,7 +69,7 @@ def main():
     print("begin train")
     train_model(FLAGS=FLAGS,decoder=decoder,
                 encoder=encoder,train_data=train_data, val_data=val_data ,optimizer=optimizer,
-                beta=args.beta,lamb=lambd,clipping=clipping,batch_size=batch_size,
+                beta=args.beta,lamb=args.lambd,clipping=clipping,batch_size=batch_size,
                 tree_encoder =not(image_tree==None), tree_decoder = not(sentence_tree==None),final=False,
                 val_all_captions=val_all_captions)
 
